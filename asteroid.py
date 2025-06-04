2448570ed6ec88b71b2a94c976619f6a09edd516
smcallister <stevenkmcallister@gmail.com>
2025-06-04 19:38:47 -0400

    Split asteroids when hit by a shot

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import pygame
+import random
 
 from circleshape import CircleShape
+from constants import *
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +13,16 @@ class Asteroid(CircleShape):
     
     def update(self, dt):
         self.position += (self.velocity * dt)
+
+    def split(self):
+        self.kill()
+        if self.radius > ASTEROID_MIN_RADIUS:
+            angle = random.uniform(20, 50)
+
+            child_radius = self.radius - ASTEROID_MIN_RADIUS
+
+            child_1 = Asteroid(self.position.x, self.position.y, child_radius)
+            child_1.velocity = self.velocity.rotate(angle) * 1.2
+
+            child_2 = Asteroid(self.position.x, self.position.y, child_radius)
+            child_2.velocity = self.velocity.rotate(-angle) * 1.2
